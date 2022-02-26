65345184f90f0d5eab72e954014f91a90e3fe58f
อาทิตย์ ขาวเอี่ยม <atit@Macintosh.local>
2022-02-27 02:01:03 +0700

    F

--- fetch.config.js
+++ fetch.config.js
@@ -1,3 +1,3 @@
 // const url = "http://node-js.thddns.net:4661";
-const url = "http://localhost:3000";
+const url = "http://api.wp123.online";
 export default url;
--- server.config.js
+++ server.config.js
@@ -1,3 +1,3 @@
 // const url = "http://node-js.thddns.net:4661";
-const serverimgurl = "http://localhost:8001";
+const serverimgurl = "https://api-img.wp123.online";
 export default serverimgurl;
