c7558fd140ccbb98bfe36b4e5685976cf8ab46bf
อาทิตย์ ขาวเอี่ยม <atit@Macintosh.local>
2022-03-12 02:52:55 +0700

    UP

--- axios.config.js
+++ axios.config.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://node-js.thddns.net:4661';
-// axios.defaults.baseURL = 'http://localhost:3000';
-axios.defaults.baseURL = 'https://api.wp123.online';
+axios.defaults.baseURL = 'http://localhost:3000';
+// axios.defaults.baseURL = 'https://api.wp123.online';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
--- fetch.config.js
+++ fetch.config.js
@@ -1,5 +1,5 @@
 // const url = "http://node-js.thddns.net:4661";
-// const url = "http://localhost:3000";
-const url = "https://api.wp123.online";
+const url = "http://localhost:3000";
+// const url = "https://api.wp123.online";
 
 export default url;
